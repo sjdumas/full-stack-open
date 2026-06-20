f77a9e53b611b9909673e30b655aa75ef6975961
S. Dumas <me@sjdumas.comm>
2026-06-20 16:17:53 -0400

    Add anecdotes exercise and update to unicafe

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -66,17 +66,46 @@ const Statistics = ({
 };
 
 const App = () => {
+	const anecdotes = [
+		'If it hurts, do it more often.',
+		'Adding manpower to a late software project makes it later!',
+		'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+		'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+		'Premature optimization is the root of all evil.',
+		'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+		'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+		'The only way to go fast, is to go well.'
+	];
+
 	const [good, setGood] = useState(0);
 	const [neutral, setNeutral] = useState(0);
 	const [bad, setBad] = useState(0);
+	const [selected, setSelected] = useState(0);
+	const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
 
 	const total = good + neutral + bad;
 	const avgScore = total === 0 ? 0 : (good - bad) / total;
 	const percentPositive = total === 0 ? 0 : (good / total) * 100;
 
+	const mostVotesIndex = votes.indexOf(Math.max(...votes));
+	const mostVotes = Math.max(...votes);
+
 	const goodFeedbackHandler = () => setGood(good + 1);
 	const neutralFeedbackHandler = () => setNeutral(neutral + 1);
 	const badFeedbackHandler = () => setBad(bad + 1);
+
+	const anecdotesHandler = () => {
+		const randomIndex = Math.floor(Math.random() * anecdotes.length);
+
+		setSelected(randomIndex);
+	};
+
+	const voteHandler = () => {
+		const newVotes = [...votes]; // Make a copy of votes
+
+		newVotes[selected] += 1; // Increment vote for selected anecdote
+		setVotes(newVotes);
+	};
 
 	return (
 		<div>
@@ -105,6 +134,31 @@ const App = () => {
 					avgScore={avgScore}
 					percentPositive={percentPositive}
 				/>
+			</div>
+			<div>
+				<h1>Anecdote of the Day</h1>
+				<p>{anecdotes[selected]}</p>
+				<p>has {votes[selected]} votes</p>
+				<Button
+					text="Vote"
+					onClick={voteHandler}
+				/>
+				<Button
+					text="Next anecdote"
+					onClick={anecdotesHandler}
+				/>
+			</div>
+			<div>
+				<h1>Anecdote with Most Votes</h1>
+				{/* If no votes exist, display a message; otherwise, display the top anecdote */}
+				{mostVotes === 0 ? (
+					<p>No votes yet. Vote on some anecdotes!</p>
+				) : (
+					<div>
+						<p>{anecdotes[mostVotesIndex]}</p>
+						<p>has {mostVotes} votes</p>
+					</div>
+				)}
 			</div>
 		</div>
 	);
