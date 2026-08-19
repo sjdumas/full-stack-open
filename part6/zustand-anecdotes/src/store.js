cd75e81286d433a6594372557c496f150f47fc41
S. Dumas <me@sjdumas.comm>
2026-08-18 20:50:39 -0400

    Add exercise 6.7 and 6.8

--- part6/zustand-anecdotes/src/store.js
+++ part6/zustand-anecdotes/src/store.js
@@ -1,24 +1,8 @@
 import { create } from 'zustand'
-
-const anecdotesAtStart = [
-	'If it hurts, do it more often',
-	'Adding manpower to a late software project makes it later!',
-	'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-	'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-	'Premature optimization is the root of all evil.',
-	'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = anecdote => ({
-	content: anecdote,
-	id: getId(),
-	votes: 0
-})
+import anecdoteService from './services/anecdotes'
 
 const useAnecdoteStore = create((set) => ({
-	anecdotes: anecdotesAtStart.map(asObject),
+	anecdotes: [],
 	filter: "",
 	actions: {
 		vote: (id) => set((state) => ({
@@ -28,10 +12,18 @@ const useAnecdoteStore = create((set) => ({
 					: anecdote
 			),
 		})),
-		add: (anecdote) => set((state) =>({
-			anecdotes: state.anecdotes.concat(anecdote),
-		})),
+		add: async (content) => {
+			const newAnecdote = await anecdoteService.createNew(content);
+
+			set((state) => ({
+				anecdotes: state.anecdotes.concat(newAnecdote),
+			}));
+		},
 		setFilter: (value) => set({ filter: value }),
+		initialize: async () => {
+			const anecdotes = await anecdoteService.getAll();
+			set(() => ({ anecdotes }));
+		},
 	},
 }));
 
@@ -44,4 +36,4 @@ export const useAnecdotes = () => {
 	);
 };
 
-export const useAnecdoteActions = () => useAnecdoteStore((state) => state.actions)
+export const useAnecdoteActions = () => useAnecdoteStore((state) => state.actions);
