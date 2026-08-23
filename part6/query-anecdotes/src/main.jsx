66a8f9fea80f7200ddecfa7a100fdae517f6178c
S. Dumas <me@sjdumas.comm>
2026-08-22 21:25:15 -0400

    Add Exercises 6.20.-6.22.

--- part6/query-anecdotes/src/main.jsx
+++ part6/query-anecdotes/src/main.jsx
@@ -1,11 +1,14 @@
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
+import { NotificationContextProvider } from "./NotificationContext";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
 	<QueryClientProvider client={queryClient}>
-		<App />
+		<NotificationContextProvider>
+			<App />
+		</NotificationContextProvider>
 	</QueryClientProvider>
 );
