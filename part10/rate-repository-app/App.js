80f5def121a851895244870e07244525c89a62cf
S. Dumas <me@sjdumas.comm>
2026-09-21 17:42:08 -0400

    feat: replace Fetch API with Apollo useQuery in useRepositories

--- part10/rate-repository-app/App.js
+++ part10/rate-repository-app/App.js
@@ -1,15 +1,21 @@
+import { ApolloProvider } from "@apollo/client/react";
 import { StatusBar } from "expo-status-bar";
 import { NativeRouter } from "react-router-native";
 
 import Main from "./src/components/Main";
+import createApolloClient from "./src/utils/apolloClient";
+
+const apolloClient = createApolloClient();
 
 const App = () => {
 	return (
 		<>
 			<StatusBar style="light" />
 			<NativeRouter>
-				<Main />
-			</NativeRouter>
+				<ApolloProvider client={apolloClient}>
+					<Main />
+				</ApolloProvider>
+			</NativeRouter >
 		</>
 	);
 };
