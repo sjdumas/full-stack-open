75a2c3df92ba00043aaa77df97f35a9004272d2d
S. Dumas <me@sjdumas.comm>
2026-09-22 19:11:42 -0400

    feat: implement sign-in mutation

--- part10/rate-repository-app/src/components/SignIn.jsx
+++ part10/rate-repository-app/src/components/SignIn.jsx
@@ -3,6 +3,7 @@ import { Formik } from "formik";
 import * as yup from "yup";
 import Text from "./Text";
 import FormikTextInput from "./FormikTextInput";
+import useSignIn from "../hooks/useSignIn";
 import theme from "../theme";
 
 const styles = StyleSheet.create({
@@ -49,8 +50,17 @@ const SignInForm = ({ onSubmit }) => {
 };
 
 const SignIn = () => {
-	const onSubmit = (values) => {
-		console.log(values);
+	const [signIn] = useSignIn();
+
+	const onSubmit = async (values) => {
+		const { username, password } = values;
+
+		try {
+			const { data } = await signIn({ username, password });
+			console.log(data);
+		} catch (e) {
+			console.log(e);
+		}
 	};
 
 	return (
